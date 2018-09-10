e8c0be903db3ee97671b86f49fc5a1535d84f898
SvetilnikST <svetilnik.stezhkina@gmail.com>
2018-09-10 16:57:58 +0300

    Обновление скрипта БД.

--- base.sql
+++ base.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Авг 09 2018 г., 16:39
+-- Время создания: Сен 10 2018 г., 16:56
 -- Версия сервера: 5.5.60-0+deb8u1
 -- Версия PHP: 5.6.33-0+deb8u1
 
@@ -19,6 +19,101 @@ SET time_zone = "+00:00";
 --
 -- База данных: `counters`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `device`
+--
+
+CREATE TABLE IF NOT EXISTS `device` (
+`id` int(10) unsigned NOT NULL,
+  `network_id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `type_id` int(11) unsigned DEFAULT NULL,
+  `unitQ_id` int(11) NOT NULL,
+  `serial` varchar(100) DEFAULT NULL,
+  `lastRequestDate` int(11) DEFAULT NULL,
+  `requestsCount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=130 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `network_device`
+--
+
+CREATE TABLE IF NOT EXISTS `network_device` (
+`id` int(11) NOT NULL,
+  `device_id` int(11) unsigned NOT NULL,
+  `ip` varchar(255) DEFAULT NULL,
+  `num_port` int(11) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=58 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `qBox_data_common`
+--
+
+CREATE TABLE IF NOT EXISTS `qBox_data_common` (
+`id` int(11) NOT NULL,
+  `serial` varchar(255) NOT NULL,
+  `unit_q` int(11) NOT NULL,
+  `time_request` int(11) NOT NULL,
+  `time_device` int(11) DEFAULT NULL,
+  `time_on` int(11) DEFAULT NULL,
+  `time_run_common` int(11) DEFAULT NULL,
+  `inStore1` tinyint(1) NOT NULL DEFAULT '0',
+  `inStore2` tinyint(1) NOT NULL DEFAULT '0',
+  `inStore3` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=68 ROW_FORMAT=DYNAMIC;
+
+--
+-- Дамп данных таблицы `qBox_data_common`
+--
+
+INSERT INTO `qBox_data_common` (`id`, `serial`, `unit_q`, `time_request`, `time_device`, `time_on`, `time_run_common`, `inStore1`, `inStore2`, `inStore3`) VALUES
+(2, '00002128', 1, 1527706940, 1525115100, 24209554, 23694812, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `qBox_data_system`
+--
+
+CREATE TABLE IF NOT EXISTS `qBox_data_system` (
+`id` int(11) NOT NULL,
+  `qBox_data_common_id` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
+  `time_run_sys` int(11) DEFAULT NULL,
+  `sigma_q` double DEFAULT NULL,
+  `q1` double DEFAULT NULL,
+  `q2` double DEFAULT NULL,
+  `q3` double DEFAULT NULL,
+  `v1` double DEFAULT NULL,
+  `v2` double DEFAULT NULL,
+  `m1` double DEFAULT NULL,
+  `m2` double DEFAULT NULL,
+  `gm1` float DEFAULT NULL,
+  `gm2` float DEFAULT NULL,
+  `gv1` float DEFAULT NULL,
+  `gv2` float DEFAULT NULL,
+  `t1` float DEFAULT NULL,
+  `t2` float DEFAULT NULL,
+  `t3` float DEFAULT NULL,
+  `p1` float DEFAULT NULL,
+  `p2` float DEFAULT NULL,
+  `p3` float DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=214 ROW_FORMAT=DYNAMIC;
+
+--
+-- Дамп данных таблицы `qBox_data_system`
+--
+
+INSERT INTO `qBox_data_system` (`id`, `qBox_data_common_id`, `number`, `time_run_sys`, `sigma_q`, `q1`, `q2`, `q3`, `v1`, `v2`, `m1`, `m2`, `gm1`, `gm2`, `gv1`, `gv2`, `t1`, `t2`, `t3`, `p1`, `p2`, `p3`) VALUES
+(3, 2, 0, 23694812, 0, 5962.916, 2379.9236, 0, 106217.9, 99938.4, 0, 0, 0, 0, 11.8299, 11.5255, 63.94, 25.14, 10, 0.3824, 0.2731, 0),
+(4, 2, 0, 23690000, 0, 5962.006, 2379.0006, 3, 106000.9, 90008.4, 0, 0, 0, 0, 11.8299, 11.5255, 63.94, 25.14, 10, 0.3824, 0.2731, 0);
 
 -- --------------------------------------------------------
 
@@ -180,6 +275,28 @@ INSERT INTO `tblUserRole` (`idUserRole`, `idUser`, `idRole`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `typeDevice`
+--
+
+CREATE TABLE IF NOT EXISTS `typeDevice` (
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=4096 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `unitQ`
+--
+
+CREATE TABLE IF NOT EXISTS `unitQ` (
+`id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AVG_ROW_LENGTH=66 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `user`
 --
 
@@ -221,6 +338,30 @@ INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_res
 --
 
 --
+-- Индексы таблицы `device`
+--
+ALTER TABLE `device`
+ ADD PRIMARY KEY (`id`), ADD KEY `FK_device_typeDevice_id` (`type_id`), ADD KEY `unitQ_id` (`unitQ_id`), ADD KEY `network_id` (`network_id`);
+
+--
+-- Индексы таблицы `network_device`
+--
+ALTER TABLE `network_device`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `qBox_data_common`
+--
+ALTER TABLE `qBox_data_common`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `qBox_data_system`
+--
+ALTER TABLE `qBox_data_system`
+ ADD PRIMARY KEY (`id`), ADD KEY `fk_qBox_system_to_data` (`qBox_data_common_id`);
+
+--
 -- Индексы таблицы `tblDepartment`
 --
 ALTER TABLE `tblDepartment`
@@ -236,7 +377,7 @@ ALTER TABLE `tblRightsItems`
 -- Индексы таблицы `tblRoleRights`
 --
 ALTER TABLE `tblRoleRights`
- ADD PRIMARY KEY (`idRoleRights`);
+ ADD PRIMARY KEY (`idRoleRights`), ADD KEY `FKsye57lsc39s04110n3colpfke` (`idRightsItem`), ADD KEY `FKaylr75pmb82l7jw3cgw5musfj` (`idRole`);
 
 --
 -- Индексы таблицы `tblRoles`
@@ -248,18 +389,50 @@ ALTER TABLE `tblRoles`
 -- Индексы таблицы `tblUserRole`
 --
 ALTER TABLE `tblUserRole`
- ADD PRIMARY KEY (`idUserRole`);
+ ADD PRIMARY KEY (`idUserRole`), ADD KEY `FKglmau4338e0dxl03c8xfqxhx` (`idUser`);
+
+--
+-- Индексы таблицы `typeDevice`
+--
+ALTER TABLE `typeDevice`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `unitQ`
+--
+ALTER TABLE `unitQ`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `FKmigkligawmvbp7s89qybn6aih` (`department`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
+--
+-- AUTO_INCREMENT для таблицы `device`
+--
+ALTER TABLE `device`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `network_device`
+--
+ALTER TABLE `network_device`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `qBox_data_common`
+--
+ALTER TABLE `qBox_data_common`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `qBox_data_system`
+--
+ALTER TABLE `qBox_data_system`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT для таблицы `tblDepartment`
 --
@@ -286,10 +459,57 @@ MODIFY `idRole` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 ALTER TABLE `tblUserRole`
 MODIFY `idUserRole` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=38;
 --
+-- AUTO_INCREMENT для таблицы `typeDevice`
+--
+ALTER TABLE `typeDevice`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `unitQ`
+--
+ALTER TABLE `unitQ`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `device`
+--
+ALTER TABLE `device`
+ADD CONSTRAINT `device_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `typeDevice` (`id`),
+ADD CONSTRAINT `device_ibfk_3` FOREIGN KEY (`unitQ_id`) REFERENCES `unitQ` (`id`),
+ADD CONSTRAINT `device_ibfk_4` FOREIGN KEY (`network_id`) REFERENCES `network_device` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `qBox_data_system`
+--
+ALTER TABLE `qBox_data_system`
+ADD CONSTRAINT `qBox_data_system_ibfk_1` FOREIGN KEY (`qBox_data_common_id`) REFERENCES `qBox_data_common` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `tblRoleRights`
+--
+ALTER TABLE `tblRoleRights`
+ADD CONSTRAINT `FKaylr75pmb82l7jw3cgw5musfj` FOREIGN KEY (`idRole`) REFERENCES `tblRoles` (`idRole`),
+ADD CONSTRAINT `FKsye57lsc39s04110n3colpfke` FOREIGN KEY (`idRightsItem`) REFERENCES `tblRightsItems` (`idRightItem`);
+
+--
+-- Ограничения внешнего ключа таблицы `tblUserRole`
+--
+ALTER TABLE `tblUserRole`
+ADD CONSTRAINT `FKglmau4338e0dxl03c8xfqxhx` FOREIGN KEY (`idUser`) REFERENCES `user` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `user`
+--
+ALTER TABLE `user`
+ADD CONSTRAINT `FKmigkligawmvbp7s89qybn6aih` FOREIGN KEY (`department`) REFERENCES `tblDepartment` (`idDepartment`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
