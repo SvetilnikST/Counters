716fc1639273a613e3117029a391bb5f3c44ad07
SvetilnikST <svetilnik.stezhkina@gmail.com>
2018-09-11 14:56:36 +0300

    AbstractDAO

--- src/main/java/pst.asu/beans/DAO.java
+++
@@ -1,9 +0,0 @@
-package pst.asu.beans;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-public class DAO {
-    @PersistenceContext(unitName = "counters-app")
-    public EntityManager entityManager;
-}
--- src/main/java/pst.asu/beans/qBox/Common/BoxCommonDAO.java
+++ src/main/java/pst.asu/beans/qBox/Common/BoxCommonDAO.java
@@ -1,20 +1,36 @@
 package pst.asu.beans.qBox.Common;
+//
+//import javax.ejb.LocalBean;
+//import javax.ejb.Stateless;
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+//@LocalBean
+//@Stateless
+//public class BoxCommonDAO  {
+//
+//    @PersistenceContext(unitName = "counters-app")
+//    private EntityManager entityManager;
+//
+//    public boolean create(TblBoxCommonEntity boxCommonEntity){
+//        entityManager.persist(boxCommonEntity);
+//        return true;
+//    }
+//
+//}
+
+
+import pst.asu.beans.AbstractDao;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @LocalBean
 @Stateless
-public class BoxCommonDAO {
+public class BoxCommonDAO extends AbstractDao<TblBoxCommonEntity> {
 
-    @PersistenceContext(unitName = "counters-app")
-    private EntityManager entityManager;
-
-    public boolean create(TblBoxCommonEntity boxCommonEntity){
-        entityManager.persist(boxCommonEntity);
-        return true;
+    public BoxCommonDAO() {
+        super(TblBoxCommonEntity.class);
     }
 
 }
