1e63a8e052346610ab8e7a2c67b500ef2d293f3d
SvetilnikST <svetilnik.stezhkina@gmail.com>
2018-09-12 16:51:01 +0300

    Необходимо написать страницы *.xhtml для таблицы typeDevice.

--- src/main/java/pst/beans/typeDevice/TypeDeviceAdministration.java
+++ src/main/java/pst/beans/typeDevice/TypeDeviceAdministration.java
@@ -1,16 +1,22 @@
 package pst.beans.typeDevice;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.inject.Default;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 @ManagedBean(name = "typeDeviceAdministration")
-public class TypeDeviceAdministration {
+@ViewScoped
+public class TypeDeviceAdministration implements Serializable {
 
-    private TblTypeDeviceEntity typeDeviceEntity;
-
-
+    private TblTypeDeviceEntity tblTypeDeviceEntity;
     private List<TypeDeviceAdministration> typeDeviceAdministrations;
 
     @EJB
@@ -20,12 +26,61 @@ public class TypeDeviceAdministration {
     private int id;
     private String name;
 
-    public TblTypeDeviceEntity getTypeDeviceEntity() {
-        return typeDeviceEntity;
+    private List<TblTypeDeviceEntity> tblTypeDeviceEntitysList;
+
+    @PostConstruct
+    void start(){
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        Map<String, String> parameterMap = (Map<String, String>) externalContext.getRequestParameterMap();
+        String param = parameterMap.get("id");
+        //если не пустой id, делаем чтение записи по id
+        if(param!=null){
+
+            id = Integer.parseInt(param);
+            //чтение записи по id
+            this.tblTypeDeviceEntity = typeDeviceDAO.read(id);
+            //проверка если пустая entity, сообщение об ощибке
+            if(tblTypeDeviceEntity==null){
+                String message = "Bad request. Unknown type device.";
+                FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,message,null));
+            }
+            //вызываем метод загрузки данный в сущность
+            load(tblTypeDeviceEntity);
+        }
+        else {
+            name="";
+        }
+        tblTypeDeviceEntitysList=typeDeviceDAO.findAll();
+
     }
 
-    public void setTypeDeviceEntity(TblTypeDeviceEntity typeDeviceEntity) {
-        this.typeDeviceEntity = typeDeviceEntity;
+    public void load(TblTypeDeviceEntity typeDeviceEntity){
+        this.setId(typeDeviceEntity.getId());
+        this.setName(typeDeviceEntity.getName());
+    }
+
+    public String save(){
+        tblTypeDeviceEntity = typeDeviceDAO.find(this.id);
+        if(tblTypeDeviceEntity==null){
+            tblTypeDeviceEntity = new TblTypeDeviceEntity();
+        }
+        tblTypeDeviceEntity.setName(this.name);
+        if(tblTypeDeviceEntity.getId()==0){
+            typeDeviceDAO.create(tblTypeDeviceEntity);
+        }
+        else {
+            typeDeviceDAO.update(tblTypeDeviceEntity);
+        }
+        return "listTypeDevice.xhtml?faces-redirect=true&id"+String.valueOf(tblTypeDeviceEntity.getId());
+    }
+
+    public TblTypeDeviceEntity getTblTypeDeviceEntity() {
+        return tblTypeDeviceEntity;
+    }
+
+    public void setTblTypeDeviceEntity(TblTypeDeviceEntity tblTypeDeviceEntity) {
+        this.tblTypeDeviceEntity = tblTypeDeviceEntity;
     }
 
     public List<TypeDeviceAdministration> getTypeDeviceAdministrations() {
@@ -58,5 +113,13 @@ public class TypeDeviceAdministration {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<TblTypeDeviceEntity> getTblTypeDeviceEntitysList() {
+        return tblTypeDeviceEntitysList;
+    }
+
+    public void setTblTypeDeviceEntitysList(List<TblTypeDeviceEntity> tblTypeDeviceEntitysList) {
+        this.tblTypeDeviceEntitysList = tblTypeDeviceEntitysList;
     }
 }
