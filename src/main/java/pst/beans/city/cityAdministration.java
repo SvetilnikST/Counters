da2ce66fb41a9da107fbf7e6e6753fb7094373a9
Svetilnik <svetilnik.stezhkina@gmail.com>
2018-11-28 21:54:36 +0300

    Реализована работа с таблицами улицы и города через сайт

---
+++ src/main/java/pst/beans/city/cityAdministration.java
@@ -0,0 +1,123 @@
+package pst.beans.city;
+
+
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.inject.Default;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean (name="cityAdministration")
+@ViewScoped
+public class cityAdministration implements Serializable {
+
+    private TblCityEntity tblCityEntity;
+    private List<cityAdministration> cityAdministrations;
+
+    @EJB
+    private CityDAO cityDAO;
+
+    @Default
+    private int idCity;
+    private String nameCity;
+
+    private List<TblCityEntity> tblCityEntitysList;
+
+    @PostConstruct
+    void start(){
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        Map<String, String> parameterMap = (Map<String, String>) externalContext.getRequestParameterMap();
+        String param = parameterMap.get("idCity");
+        if (param != null) {
+            idCity = Integer.parseInt(param);
+            this.tblCityEntity = cityDAO.read(idCity);
+            if (tblCityEntity == null) {
+                String message = "Bad request. Unknown task.";
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+            }
+            load(tblCityEntity);
+
+        } else {
+            this.nameCity = "";
+        }
+        tblCityEntitysList = cityDAO.findAll();
+    }
+
+
+    public void load(TblCityEntity cityEntity) {
+        this.setIdCity(cityEntity.getIdCity());
+        this.setNameCity(cityEntity.getNameCity());
+    }
+
+    public String save() {
+        tblCityEntity = cityDAO.find(this.idCity);
+        if (tblCityEntity == null) {
+            tblCityEntity = new TblCityEntity();
+        }
+        tblCityEntity.setNameCity(this.nameCity);
+        if (tblCityEntity.getIdCity() == 0) {
+
+            cityDAO.create(tblCityEntity);
+        } else {
+            cityDAO.update(tblCityEntity);
+        }
+        return "listCity.xhtml?faces-redirect=true&id=" + String.valueOf(tblCityEntity.getIdCity());
+    }
+
+    public TblCityEntity getTblCityEntity() {
+        return tblCityEntity;
+    }
+
+    public void setTblCityEntity(TblCityEntity tblCityEntity) {
+        this.tblCityEntity = tblCityEntity;
+    }
+
+    public List<cityAdministration> getCityAdministrations() {
+        return cityAdministrations;
+    }
+
+    public void setCityAdministrations(List<cityAdministration> cityAdministrations) {
+        this.cityAdministrations = cityAdministrations;
+    }
+
+    public CityDAO getCityDAO() {
+        return cityDAO;
+    }
+
+    public void setCityDAO(CityDAO cityDAO) {
+        this.cityDAO = cityDAO;
+    }
+
+    public int getIdCity() {
+        return idCity;
+    }
+
+    public void setIdCity(int idCity) {
+        this.idCity = idCity;
+    }
+
+    public String getNameCity() {
+        return nameCity;
+    }
+
+    public void setNameCity(String nameCity) {
+        this.nameCity = nameCity;
+    }
+
+    public List<TblCityEntity> getTblCityEntitysList() {
+        return tblCityEntitysList;
+    }
+
+    public void setTblCityEntitysList(List<TblCityEntity> tblCityEntitysList) {
+        this.tblCityEntitysList = tblCityEntitysList;
+    }
+}
---
+++ src/main/java/pst/beans/street/streetAdministration.java
@@ -0,0 +1,123 @@
+package pst.beans.street;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.inject.Default;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean (name="streetAdministration")
+@ViewScoped
+public class streetAdministration implements Serializable {
+
+    private TblStreetEntity tblStreetEntity;
+    private List<streetAdministration> streetAdministrations;
+
+    @EJB
+    private StreetDAO streetDAO;
+
+    @Default
+    private int idStreet;
+    private String nameStreet;
+
+    private List<TblStreetEntity> tblStreetEntitysList;
+
+    @PostConstruct
+    void start(){
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        Map<String, String> parameterMap = (Map<String, String>) externalContext.getRequestParameterMap();
+        String param = parameterMap.get("idStreet");
+        if (param != null) {
+            idStreet = Integer.parseInt(param);
+            this.tblStreetEntity = streetDAO.read(idStreet);
+            if (tblStreetEntity == null) {
+                String message = "Bad request. Unknown task.";
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+            }
+            load(tblStreetEntity);
+
+        } else {
+            this.nameStreet = "";
+        }
+        tblStreetEntitysList = streetDAO.findAll();
+    }
+
+
+    public void load(TblStreetEntity streetEntity) {
+        this.setIdStreet(streetEntity.getIdStreet());
+        this.setNameStreet(streetEntity.getNameStreet());
+    }
+
+    public String save() {
+        tblStreetEntity = streetDAO.find(this.idStreet);
+        if (tblStreetEntity == null) {
+            tblStreetEntity = new TblStreetEntity();
+        }
+        tblStreetEntity.setNameStreet(this.nameStreet);
+        if (tblStreetEntity.getIdStreet() == 0) {
+
+            streetDAO.create(tblStreetEntity);
+        } else {
+            streetDAO.update(tblStreetEntity);
+        }
+        return "listStreet.xhtml?faces-redirect=true&id=" + String.valueOf(tblStreetEntity.getIdStreet());
+    }
+
+
+    public TblStreetEntity getTblStreetEntity() {
+        return tblStreetEntity;
+    }
+
+    public void setTblStreetEntity(TblStreetEntity tblStreetEntity) {
+        this.tblStreetEntity = tblStreetEntity;
+    }
+
+    public List<streetAdministration> getStreetAdministrations() {
+        return streetAdministrations;
+    }
+
+    public void setStreetAdministrations(List<streetAdministration> streetAdministrations) {
+        this.streetAdministrations = streetAdministrations;
+    }
+
+    public StreetDAO getStreetDAO() {
+        return streetDAO;
+    }
+
+    public void setStreetDAO(StreetDAO streetDAO) {
+        this.streetDAO = streetDAO;
+    }
+
+    public int getIdStreet() {
+        return idStreet;
+    }
+
+    public void setIdStreet(int idStreet) {
+        this.idStreet = idStreet;
+    }
+
+    public String getNameStreet() {
+        return nameStreet;
+    }
+
+    public void setNameStreet(String nameStreet) {
+        this.nameStreet = nameStreet;
+    }
+
+    public List<TblStreetEntity> getTblStreetEntitysList() {
+        return tblStreetEntitysList;
+    }
+
+    public void setTblStreetEntitysList(List<TblStreetEntity> tblStreetEntitysList) {
+        this.tblStreetEntitysList = tblStreetEntitysList;
+    }
+
+}
